66ea25266e6fa7827915e2feeb57c3524ff7970b
Marina Nicolai <nicolaimarina@gmail.com>
2020-07-05 08:44:33 -0400

    Footer styling

--- recipe/src/components/Footer.js
+++ recipe/src/components/Footer.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
-      <p>Created by Marina Nicolai &copy; 2020.</p>
+    <footer className="pt-5">
+      <div className="text-center">
+        <p>Created by Marina Nicolai &copy; 2020.</p>
+      </div>
     </footer>
   );
 }
